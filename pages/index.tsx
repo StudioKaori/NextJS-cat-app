cea58abc4b9cf74e32766d15d7f8dd306a83631a
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-29 10:44:29 +0200

    Add api

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,13 @@ import styles from '../styles/Home.module.css'
 
 // NextPage -> type, line it;s imported from next 
 const Home: NextPage = () => {
+
+  const fetchCatImage = async () => {
+    const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=1');
+    const result = await res.json();
+    console.log(result);
+  };
+
   return (
     <div style={{
       display: 'flex',
@@ -14,7 +21,7 @@ const Home: NextPage = () => {
     }}>
       <h1>Cat images</h1>
       <img src='https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg' width={500} height={'auto'} />
-      <button>Cat</button>
+      <button onClick={fetchCatImage}>Cat</button>
     </div>
   )
 }
