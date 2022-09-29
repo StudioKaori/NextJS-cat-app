6ee8353a6f2d7dddeb369d15e2d055b22c7b012b
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-29 11:09:51 +0200

    Add indexpageprops

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react';
 import styles from '../styles/Home.module.css'
 
+// SSG Static site generator 静的生成、ビルドした時に既にデータを用意。読み込み早い。非同期通信にはしようできない。
+// SSR server side props リクエストごとに生成。今回はこちらを使用。
 
 interface SearchCatImage {
   id: string;
@@ -12,22 +14,27 @@ interface SearchCatImage {
   height: number;
 }
 
+interface IndexPageProps {
+  initialCatImageUrl: string;
+}
+
+// To use for SSR, should be outside of return
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+  const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=1');
+  const result = await res.json();
+  //console.log(result[0]);
+  return result[0];
+};
+
 // NextPage -> type, line it;s imported from next 
-const Home: NextPage = () => {
+const Home: NextPage<IndexPageProps> = ({initialCatImageUrl}) => {
 
-  const [catImageUrl, setCatImageUrl] = useState('');
-
-  const fetchCatImage = async (): Promise<SearchCatImage> => {
-    const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=1');
-    const result = await res.json();
-    //console.log(result[0]);
-    return result[0];
-  };
+  const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl);
 
   const handleClick = async () => {
     const catImage = await fetchCatImage();
     setCatImageUrl(catImage.url);
-  }
+  };
 
   return (
     <div style={{
@@ -41,6 +48,17 @@ const Home: NextPage = () => {
       <button onClick={handleClick}>Cat</button>
     </div>
   )
-}
+};
+
+// For SSR, server side rendering
+// The cat image url will be returned. Inside of the generics
+export const getServerSideProps: GetServerSideProps<IndexPageProps> =async () => {
+  const catImage = await fetchCatImage();
+  return {
+    props: {
+      initialCatImageUrl: catImage.url,
+    },
+  };
+};
 
 export default Home
